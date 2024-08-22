6d8b184adb217e961cb0249c20d054a2770eae6a
TbadolStation <tbadolstation2024@gmail.com>
2024-08-23 00:02:35 +0300

    Updated Heroku configuration

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Http\Controllers\ProductController::class,'create'])->name('home');
-Route::post('/', [\App\Http\Controllers\ProductController::class,'store'])->name('home.store');
+Route::get('/', [\App\Http\Controllers\ProductController::class,'create'])->name('index');
+Route::post('/', [\App\Http\Controllers\ProductController::class,'store'])->name('index.store');
 Route::get('/showProduct',[\App\Http\Controllers\ProductController::class,'index'])->name('showProduct');
